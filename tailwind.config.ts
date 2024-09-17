f81dddf23461a9b203e09cbe58a66fca9b40abce
hugoomdra <hugoomdra@gmail.com>
2024-09-17 17:49:52 +0200

    wip

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      backgroundImage: {
+        'clement': "url('/assets/img/clement.jpg')",
+      },
     },
   },
   plugins: [],
